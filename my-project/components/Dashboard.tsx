af328898c85febe8b0c553f257614acb0ac74d25
Pak Shing Kan <kennethkankps@gmail.com>
2024-05-18 00:05:28 -0400

    remove img

--- my-project/components/Dashboard.tsx
+++ my-project/components/Dashboard.tsx
@@ -10,10 +10,6 @@ const Dashboard = () => {
     <>
       {session ? (
         <>
-          <img
-            src={session.user?.image as string}
-            className="rounded-full h-20 w-20"
-          ></img>
           <h1 className="text-3xl text-green-500 font-bold">
             Welcome back, {session.user?.name}
           </h1>
