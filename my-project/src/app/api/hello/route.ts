40595dc315ad69f5f54381895b605f7ed23dc00e
Pak Shing Kan <kennethkankps@gmail.com>
2024-07-08 14:45:35 -0400

    testing

--- my-project/src/app/api/hello/route.ts
+++ my-project/src/app/api/hello/route.ts
@@ -1,4 +1,4 @@
 import { NextResponse, NextRequest } from 'next/server'
 export async function GET(req: Request, res: NextResponse){
-    return NextResponse.json({ message: process.env.GITHUB_CLIENT_ID });
+    return NextResponse.json({ message: process.env.NEXTAUTH_URL });
 }
