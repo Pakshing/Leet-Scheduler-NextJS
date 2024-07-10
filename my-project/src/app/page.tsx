5b79566993b9752a072eb33cc7986c54d77fe70f
Pak Shing Kan <kennethkankps@gmail.com>
2024-07-10 13:26:14 -0400

    new home page png, and home page styling changed

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
 
 
   return (
-    <div className="flex justify-center items-center bg-gray-300 h-screen" >
+    <div className="flex justify-center items-center bg-gray-300 h-screen">
         <div className="content-section m-12 w-auto h-4/5 bg-theme-color text-center flex rounded-lg">
-          <div className="intro-text flex-1 ml-8 sm:mr-4 text-white items-center justify-center flex flex-col leading-10 w-full">
-            <div className="max-w-xl">
-              <h1 className="font-extrabold text-4xl mb-10 ">LeetCode Scheduler</h1>
-              <p className="text-xl leading-relaxed pb-10">Introducing Leetcode Scheduler! This application facilitates the tracking of your Leetcode question completions and offers scheduling based on spaced-repetition learning techniques to optimize your learning process. Embark on your journey to Leetcode mastery today!</p>
-              <OauthLoginDialog/>
+            <div className="intro-text w-2/5 ml-8 sm:mr-8 text-white items-center justify-center flex flex-col leading-10">
+                <div className="max-w-xl">
+                    <h1 className="font-extrabold text-4xl mb-10">LeetCode Scheduler</h1>
+                    <p className="text-xl leading-relaxed pb-10">Introducing Leetcode Scheduler! This application facilitates the tracking of your Leetcode question completions and offers scheduling based on spaced-repetition learning techniques to optimize your learning process. Embark on your journey to Leetcode mastery today!</p>
+                    <OauthLoginDialog/>
+                </div>
             </div>
-          </div>
-          <div className="hidden lg:flex flex-1 items-center mr-10 rounded-lg ">
-            <Image  src="/homeQuestions.png" alt="Questions"  width={1000} height={1000}  />
-          </div>
+            <div className="hidden lg:flex w-3/5 items-center justify-center mr-10 rounded-lg">
+                <Image src="/homeQuestions.png" alt="Questions" width={1200} height={1200} />
+            </div>
         </div>
     </div>
   );
