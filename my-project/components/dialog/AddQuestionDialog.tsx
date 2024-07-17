befa30ae42f2dec1482d01e68b437c47cbbad352
Pak Shing Kan <kennethkankps@gmail.com>
2024-07-16 21:35:15 -0400

    fix all the dialog mobile responsive styling problem

--- my-project/components/dialog/AddQuestionDialog.tsx
+++ my-project/components/dialog/AddQuestionDialog.tsx
@@ -113,107 +113,112 @@ const AddQuestionDialog:React.FC<AddQuestionDialogProps> = ({refresh}) => {
       }
     }
   }
-
-
-
-
-
- 
   return (
     <>
-      <Button className="flex items-center gap-3" size="sm" onClick={handleOpen}>
-          <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add Question
-        </Button>
+      <Button className="flex items-center gap-2 text-xs sm:text-sm" size="sm" onClick={handleOpen}>
+        <UserPlusIcon strokeWidth={2} className="h-3 w-3 sm:h-4 sm:w-4" /> Add Question
+      </Button>
       
-      <Dialog open={open} handler={handleOpen} size="lg" className="p-10">
-        
-        <DialogBody>
-        <Card color="transparent" shadow={false}  className="flex items-center justify-center">
-      <form className="mb-2" onSubmit={()=>handleFormSubmit}>
-        <div className="mb-1 flex flex-col gap-6">
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            LeetCode URL
-          </Typography>
-          <Input
-            size="lg"
-            placeholder="https://leetcode.com/problems/two-sum/description"
-            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-            crossOrigin={""}
-            value={inputUrl.url}
-            onChange={(e) => handleUrlInput(e.target.value)}
-            error={inputUrl.url !== "" && inputUrl.format === false?true:false}
-          />
-          <Typography
-          variant="small"
-          color="gray"
-          className=" flex items-center gap-1 font-normal"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="-mt-px h-4 w-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-          Please follow this LeetCode Url format: https://leetcode.com/problems/two-sum/***
-      </Typography>
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            Difficulty
-          </Typography>
-          <ButtonGroup ripple={true} variant="outlined">
-            <Button onClick={()=>handleDfficultyChange("EASY")} className={difficulty==="EASY"?'bg-black text-white':''}>EASY</Button>
-            <Button onClick={()=>handleDfficultyChange("MEDIUM")} className={difficulty === 'MEDIUM' ? 'bg-black text-white' : ''}>MEDIUM</Button>
-            <Button onClick={()=>handleDfficultyChange("HARD")}className={difficulty === 'HARD' ? 'bg-black text-white' : ''}>HARD</Button>
-          </ButtonGroup>
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            Tags
-          </Typography>
-          <div className="flex flex-wrap -m-1">
-            {questionTags.map((tag) => (
-              <Button 
-                key={tag} 
-                variant="outlined"
-                onClick={() => handleCheckboxChange(tag)} 
-                className={`m-1 py-2 px-3 text-sm ${checkedTags.includes(tag) ? 'bg-black text-white' : 'bg-white text-black'}`}
+      <Dialog 
+        open={open} 
+        handler={handleOpen} 
+        size="lg" 
+        className="max-h-[90vh] w-[95%] sm:w-[85%] md:w-[75%] lg:w-[60%] flex flex-col"
+      >
+        <DialogBody className="p-2 sm:p-4 overflow-y-auto flex-grow">
+          <form className="mb-2" onSubmit={(e) => { e.preventDefault(); handleFormSubmit(); }}>
+            <div className="flex flex-col gap-4 sm:gap-6">
+              <Typography variant="h6" color="blue-gray" className="text-base sm:text-lg">
+                LeetCode URL
+              </Typography>
+              <Input
+                size="lg"
+                placeholder="https://leetcode.com/problems/two-sum/description"
+                className="!border-t-blue-gray-200 focus:!border-t-gray-900 text-sm sm:text-base"
+                crossOrigin={""}
+                value={inputUrl.url}
+                onChange={(e) => handleUrlInput(e.target.value)}
+                error={inputUrl.url !== "" && inputUrl.format === false}
+              />
+              <Typography
+                variant="small"
+                color="gray"
+                className="flex items-center gap-1 font-normal text-xs sm:text-sm"
               >
-                {tag}
-              </Button>
-            ))}
-          </div>
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            Next review date
-          </Typography>
-          <ButtonGroup ripple={true} variant="outlined">
-            {daysToReview.map((day) => (
-              <Button key={day} onClick={() => handleDaysReviewChange(day)} className={daysReview === day ? 'bg-black text-white' : ''}>
-                {day} days
-              </Button>
-            ))}
-            <Button onClick={() => handleDaysReviewChange(0)} className={daysReview === 0 ? 'bg-black text-white' : ''}>
-              Never
-            </Button>
-          </ButtonGroup>
-        </div>
-      </form>
-    </Card>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-3 w-3 sm:h-4 sm:w-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Please follow this LeetCode Url format: https://leetcode.com/problems/two-sum/***
+              </Typography>
+              <Typography variant="h6" color="blue-gray" className="text-base sm:text-lg mt-2">
+                Difficulty
+              </Typography>
+              <div className="flex flex-wrap gap-2">
+                {["EASY", "MEDIUM", "HARD"].map((diff) => (
+                  <Button
+                    key={diff}
+                    onClick={() => handleDfficultyChange(diff)}
+                    variant="outlined"
+                    className={`text-xs sm:text-sm px-2 py-1 sm:px-4 sm:py-2 ${difficulty === diff ? 'bg-black text-white' : 'bg-white text-black'}`}
+                  >
+                    {diff}
+                  </Button>
+                ))}
+              </div>
+              <Typography variant="h6" color="blue-gray" className="text-base sm:text-lg mt-2">
+                Tags
+              </Typography>
+              <div className="flex flex-wrap gap-2">
+                {questionTags.map((tag) => (
+                  <Button 
+                    key={tag} 
+                    variant="outlined"
+                    onClick={() => handleCheckboxChange(tag)} 
+                    className={`text-xs sm:text-sm px-2 py-1 sm:px-3 sm:py-2 ${checkedTags.includes(tag) ? 'bg-black text-white' : 'bg-white text-black'}`}
+                  >
+                    {tag}
+                  </Button>
+                ))}
+              </div>
+              <Typography variant="h6" color="blue-gray" className="text-base sm:text-lg mt-2">
+                Next review date
+              </Typography>
+              <div className="flex flex-wrap gap-2 ">
+                {[...daysToReview, 0].map((day) => (
+                  <Button 
+                    key={day} 
+                    onClick={() => handleDaysReviewChange(day)} 
+                    variant="outlined"
+                    className={`text-xs sm:text-sm px-2 py-1 sm:px-4 sm:py-2 ${daysReview === day ? 'bg-black text-white' : 'bg-white text-black'}`}
+                  >
+                    {day === 0 ? 'Never' : `${day} days`}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </form>
         </DialogBody>
-        <DialogFooter>
-          <div className="flex gap-4">
-          <Button variant="outlined" onClick={handleCancelOnClick} size="sm">
-            <span>Cancel</span>
-          </Button>
-          <Button variant="gradient"  onClick={handleFormSubmit} size="sm">
-            <span>Submit</span>
-          </Button>
+        <DialogFooter className="p-2 sm:p-4">
+          <div className="flex gap-2 sm:gap-4">
+            <Button variant="outlined" onClick={handleCancelOnClick} size="sm" className="text-xs sm:text-sm">
+              <span>Cancel</span>
+            </Button>
+            <Button variant="gradient" onClick={handleFormSubmit} size="sm" className="text-xs sm:text-sm">
+              <span>Submit</span>
+            </Button>
           </div>
         </DialogFooter>
       </Dialog>
-      </>
+    </>
   );
 }
 
--- my-project/components/dialog/NextReviewDialog.tsx
+++ my-project/components/dialog/NextReviewDialog.tsx
@@ -1,71 +1,48 @@
 'use client'
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {
   Button,
   Dialog,
-  DialogHeader,
   DialogBody,
   DialogFooter,
-  Input,
   Typography,
-  Checkbox,
-  Card,
-  ButtonGroup,
-  List, 
-  ListItem,
-  ListItemPrefix,
 } from "@material-tailwind/react";
-import { questionTags,daysToReview } from "../../types/type";
-import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { QuestionType } from "../../types/type";
-import Link from "next/link";
+import { questionTags, daysToReview, QuestionType } from "../../types/type";
 
 interface NextReviewDialogProps {
-    question: QuestionType;
-    refresh : () => void
+  question: QuestionType;
+  refresh: () => void;
 }
 
-type QuestionInput = {
-    id?: number,
-    title?: string,
-    url: string,
-    daysReview: number
-    difficulty: string,
-    tags: string[],
-    ownerId: string
-}
-
-
-const NextReviewDialog:React.FC<NextReviewDialogProps> = ({question,refresh}) => {
+const NextReviewDialog: React.FC<NextReviewDialogProps> = ({ question, refresh }) => {
   const [open, setOpen] = useState(false);
   const [daysReview, setDaysReview] = useState<number>();
  
   const handleOpen = () => setOpen(!open);
 
-
-  const handleDaysReviewChange = (value:number) => {
+  const handleDaysReviewChange = (value: number) => {
     setDaysReview(value);
   }
 
-  const resetForm = () =>{
-    setDaysReview(undefined)
+  const resetForm = () => {
+    setDaysReview(undefined);
   }
 
-  const handleFormSubmit = async() => {
-    if(daysReview === undefined) alert("Please select a review date")
-    else{
+  const handleFormSubmit = async () => {
+    if (daysReview === undefined) {
+      alert("Please select a review date");
+    } else {
       try {
         const updatedQuestion = {
-            id: question.id,
-            title: question.title,
-            url: question.url,
-            daysReview: daysReview,
-            difficulty: question.difficulty,
-            tags: question.tags,
-            ownerId: question.ownerId,
-            hasCompleted:true
-        }
+          id: question.id,
+          title: question.title,
+          url: question.url,
+          daysReview: daysReview,
+          difficulty: question.difficulty,
+          tags: question.tags,
+          ownerId: question.ownerId,
+          hasCompleted: true
+        };
         const response = await fetch(`/api/questions`, {
           method: 'PUT',
           credentials: 'include', 
@@ -74,65 +51,70 @@ const NextReviewDialog:React.FC<NextReviewDialogProps> = ({question,refresh}) =>
           },
           body: JSON.stringify(updatedQuestion)
         }).then((res) => res.json());
-        if(response.status === 400){
-          throw new Error("An error occured")
+        if (response.status === 400) {
+          throw new Error("An error occurred");
         }
-        refresh()
-        resetForm()
-        setOpen(false)
+        refresh();
+        resetForm();
+        setOpen(false);
       } catch (error) {
-        alert("An error occured")
+        alert("An error occurred");
       }
     }
   }
 
-
-
-
-
- 
   return (
     <>
-      
-        <a href={question.url} target="_blank" rel="noopener noreferrer" >
-            <Button className="px-2" variant="text" size="sm" onClick={handleOpen}>{question.title} </Button>
-        </a>
+      <a href={question.url} target="_blank" rel="noopener noreferrer" className="inline-block">
+        <Button 
+          className="px-2 py-1 text-xs sm:text-sm md:text-base whitespace-normal text-left" 
+          variant="text" 
+          size="sm" 
+          onClick={handleOpen}
+        >
+          {question.title}
+        </Button>
+      </a>
           
-      <Dialog open={open} handler={handleOpen} size="lg" className="p-10">
-        
-        <DialogBody>
-        <Card color="transparent" shadow={false}  className="flex items-center justify-center">
-      <form className="mb-2" onSubmit={()=>handleFormSubmit}>
-        <div className="mb-1 flex flex-col gap-6">
-          <Typography variant="h6" color="blue-gray" className="-mb-3">
-            Next review date
-          </Typography>
-          <ButtonGroup ripple={true} variant="outlined">
-            {daysToReview.map((day) => (
-              <Button key={day} onClick={() => handleDaysReviewChange(day)} className={daysReview === day ? 'bg-black text-white' : ''}>
-                {day} days
-              </Button>
-            ))}
-            <Button onClick={() => handleDaysReviewChange(0)} className={daysReview === 0 ? 'bg-black text-white' : ''}>
-              Never
-            </Button>
-          </ButtonGroup>
-        </div>
-      </form>
-    </Card>
+      <Dialog 
+        open={open} 
+        handler={handleOpen} 
+        size="md" 
+        className="flex flex-col max-w-[95%] sm:max-w-[85%] md:max-w-[75%] lg:max-w-[60%] max-h-[90vh]"
+      >
+        <DialogBody className="p-2 sm:p-4 overflow-y-auto">
+          <form className="mb-2" onSubmit={(e) => { e.preventDefault(); handleFormSubmit(); }}>
+            <div className="mb-1 flex flex-col gap-4 sm:gap-6">
+              <Typography variant="h6" color="blue-gray" className="text-base sm:text-lg md:text-xl">
+                Next review date
+              </Typography>
+              <div className="flex flex-wrap gap-2">
+                {[...daysToReview, 0].map((day) => (
+                  <Button 
+                    key={day} 
+                    onClick={() => handleDaysReviewChange(day)} 
+                    variant="outlined"
+                    className={`text-xs sm:text-sm md:text-base px-2 py-1 sm:px-4 sm:py-2 ${daysReview === day ? 'bg-black text-white' : 'bg-white text-black'}`}
+                  >
+                    {day === 0 ? 'Never' : `${day} days`}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </form>
         </DialogBody>
-        <DialogFooter>
-          <div className="flex gap-4">
-          <Button variant="outlined" onClick={handleOpen} size="sm">
-            <span>Cancel</span>
-          </Button>
-          <Button variant="gradient"  onClick={handleFormSubmit} size="sm">
-            <span>Submit</span>
-          </Button>
+        <DialogFooter className="p-2 sm:p-4">
+          <div className="flex gap-2 sm:gap-4">
+            <Button variant="outlined" onClick={handleOpen} size="sm" className="text-xs sm:text-sm">
+              <span>Cancel</span>
+            </Button>
+            <Button variant="gradient" onClick={handleFormSubmit} size="sm" className="text-xs sm:text-sm">
+              <span>Submit</span>
+            </Button>
           </div>
         </DialogFooter>
       </Dialog>
-      </>
+    </>
   );
 }
 
